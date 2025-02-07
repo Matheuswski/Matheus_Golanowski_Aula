41ee430eca0369dcba0a65f003147bc98172200d
MatheusG <mariaoglanowski@gmail.com>
2025-02-06 22:03:59 -0300

    alteração exercicio 03
    salario com aumento de 5%

--- Exe03.py
+++ Exe03.py
@@ -1,1 +1,6 @@
-# Aula 3 de Python
+# Faça um programa que receba um salario de R$1500 tenha um aumento de 5% e ao final exiba o salario com o aumento
+
+salario = 1500
+x = 0.5 * salario
+real = salario + x
+print(real)

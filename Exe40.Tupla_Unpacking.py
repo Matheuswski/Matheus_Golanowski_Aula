b2fbe53aff47f94918ff4feffff42fb0d682b7f6
MatheusG <mariaoglanowski@gmail.com>
2025-02-20 18:54:20 -0300

    atualizção

---
+++ Exe40.Tupla_Unpacking.py
@@ -0,0 +1,7 @@
+vendas = ("Matheus", "24/06/2026", "16/02/2009", 4000, "Estagiario")
+nome, data_contrato, data_nacimento, salario, cargo = vendas
+print(nome)
+print(data_contrato)
+print(data_nacimento)
+print(salario)
+print(cargo)

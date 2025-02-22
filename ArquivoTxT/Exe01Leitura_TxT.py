d3367df9df13e60392002fe25238bd2624b1f3f8
MatheusG <mariaoglanowski@gmail.com>
2025-02-21 21:09:01 -0300

    Atualização

---
+++ ArquivoTxT/Exe01Leitura_TxT.py
@@ -0,0 +1,3 @@
+arquivo = open('C:/Users/Matheus_Golanowski/Documents/Pasta_txt/Alunostxt.txt','r')
+linhas = arquivo.readlines()
+print(linhas)
---
+++ ArquivoTxT/Exe03espaço.3.txt.py
@@ -0,0 +1,4 @@
+with open('C:/Users/Matheus_Golanowski/Documents/Pasta_txt/Resumo 2.txt','w') as arquivo3:
+    arquivo3.write('Escrevendo no arquivo\n')
+    arquivo3.write('Capitão Stoltomayer\n')
+print('Fim do program')
